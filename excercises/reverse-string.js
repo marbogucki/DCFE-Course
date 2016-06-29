e0c56e6297033bfaea1923020ac40e59935c5774
Marcin Bogucki <mar.bogucki@gmail.com>
2016-06-29 10:39:39 +0200

    delete comments change console log to return

--- excercises/reverse-string.js
+++ excercises/reverse-string.js
@@ -1,10 +1,6 @@
-/**
- * Created by Marcin on 2016-06-08.
- */
-
 function reverseString(txt) {
     var textNew = txt.split('').reverse().join('');
-    console.log(textNew);
+    return textNew;
 }
 
 reverseString("hello world");
