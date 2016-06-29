532d8f92de079ed4ae5e6e50ffd3ea67ceeaca6f
Marcin Bogucki <mar.bogucki@gmail.com>
2016-06-29 10:43:21 +0200

    delete comments delete var filterArray

--- excercises/destroyer.js
+++ excercises/destroyer.js
@@ -1,16 +1,10 @@
-/**
- * Created by Marcin on 2016-06-15.
- */
-
 function destroyer(arrNumbers) {
     var arrayOfArguments = Array.prototype.slice.call(arguments);
     var arrayNumber = arrayOfArguments.shift();
-    var filterArray  = arrayNumber.filter(deleteNumber);
-
     function deleteNumber(arrayNumber) {
     	 return arrayOfArguments.indexOf(arrayNumber) < 0;
     }
-    return filterArray;
+    return arrayNumber.filter(deleteNumber);
 }
 
 destroyer([2, 5, 6, 7, 3], 2, 3, 5);
