6d61bd1359a21b9d8ea4eebe68d524ae89bef2b9
Marcin Bogucki <mar.bogucki@gmail.com>
2016-06-29 10:41:12 +0200

    delete comments add white space

--- excercises/factorial.js
+++ excercises/factorial.js
@@ -1,11 +1,8 @@
-/**
- * Created by Marcin on 2016-06-09.
- */
 function factorial(num) {
     var result = 1;
-    for(var i = 1; i<=num; i++) {
+    for(var i = 1; i <= num; i++) {
         result *= i;
     }
     return result;
 }
-console.log(factorial(5));
+factorial(5);
