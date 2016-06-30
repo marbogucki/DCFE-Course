d7b0149e7c54a504a92b16e80d5ba84142940511
Marcin Bogucki <mar.bogucki@gmail.com>
2016-06-30 12:58:41 +0200

    used forEach

--- excercises/find-longest-word.js
+++ excercises/find-longest-word.js
@@ -1,15 +1,12 @@
 function findLongestWord(sentence) {
     var words = sentence.split(' ');
-    var maxLengthEl = words[0].length;
-    var myNewElement;
-    
-    var wordsLength = words.length;
-    for(var i = 0; i < wordsLength; i++) {
-        if(words[i].length > maxLengthEl) {
-            maxLengthEl = words[i].length;
-            myNewElement = words[i];
+    var myNewElement = "";
+    function longestWord(element) {
+        if(element.length > myNewElement.length) {
+            myNewElement = element;
         }
     }
+    words.forEach(longestWord);
     return myNewElement;
 }
 findLongestWord("Uczę się dobrze programować w JavaScript");
