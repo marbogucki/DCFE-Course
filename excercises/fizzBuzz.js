8f00f83a56c4801aac7c51a9fa563f640f888d5c
Marcin Bogucki <mar.bogucki@gmail.com>
2016-07-04 09:39:34 +0200

    add white space

--- excercises/fizzBuzz.js
+++ excercises/fizzBuzz.js
@@ -1,10 +1,10 @@
 function fizzBuzz() {
    for(var i = 1; i <= 100; i++) {
        var info = "";
-       if( !(i % 3) ) {
+       if( !( i % 3 ) ) {
            info += "Fizz ";
        }
-       if(!(i%5)) {
+       if( ! ( i % 5 ) ) {
            info += "Buzz ";
        }
        console.log(info || i);
